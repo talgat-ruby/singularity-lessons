e9d4d95e6376a0852ce720d268416c96f4780f9f
Talgat S <talgat.s@protonmail.com>
2022-04-28 11:09:02 +0600

    Add console.log

--- lesson4/task1.js
+++ lesson4/task1.js
@@ -1,9 +1,9 @@
 function some(arr, fn) {}
 
-some([1, 2, 3, 4], (val) => val > 2); // true
-some([1, 2, 3, 4], (val) => val > 5); // false
+console.log(some([1, 2, 3, 4], (val) => val > 2)); // true
+console.log(some([1, 2, 3, 4], (val) => val > 5)); // false
 
 function every(arr, fn) {}
 
-every([1, 2, 3, 4], (val) => val > 2); // false
-every([1, 2, 3, 4], (val) => val < 5); // true
+console.log(every([1, 2, 3, 4], (val) => val > 2)); // false
+console.log(every([1, 2, 3, 4], (val) => val < 5)); // true
