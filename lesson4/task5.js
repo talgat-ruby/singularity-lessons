e335b4bbd85ae72ffda1539140e879ed43ce9abb
Talgat S <talgat.s@protonmail.com>
2022-04-28 11:52:27 +0600

    Add task5

---
+++ lesson4/task5.js
@@ -0,0 +1,5 @@
+function filterNonUnique(arr) {}
+
+console.log(filterNonUnique([1, 2, 2, 3, 4, 4, 5])); // [1, 3, 5]
+console.log(filterNonUnique([1, 2, 3, 4])); // [1, 2, 3, 4]
+console.log(filterNonUnique([1, 2, 1, 3, 3, 4, 2])); // [4]
