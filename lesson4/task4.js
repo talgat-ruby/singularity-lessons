da0fd66815540d17d56ca72aa7b55a1143a7b544
Talgat S <talgat.s@protonmail.com>
2022-04-28 11:52:08 +0600

    Add task4

---
+++ lesson4/task4.js
@@ -0,0 +1,5 @@
+function oneOfKind(arr) {}
+
+console.log(oneOfKind([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(oneOfKind([1, 2, 3, 4])); // [1, 2, 3, 4]
+console.log(oneOfKind([1, 2, 1, 3, 3, 4, 2])); // [1, 2, 3, 4]
