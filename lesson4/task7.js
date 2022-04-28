29b2b680ae2f4ae362f346f16e7bf2f8a8ce2abf
Talgat S <talgat.s@protonmail.com>
2022-04-28 13:28:12 +0600

    Update task7

--- lesson4/task7.js
+++ lesson4/task7.js
@@ -1,22 +1,28 @@
+const wait = (delay) => new Promise((res) => setTimeout(res, delay));
+
 async function promisAll(fns) {}
 
-function fn1() {
+async function fn1() {
+	await wait(1000);
 	return Promise.resolve(1);
 }
-function fn2() {
+async function fn2() {
+	await wait(2000);
 	return Promise.resolve(2);
 }
-function fn3() {
+async function fn3() {
+	await wait(3000);
 	return Promise.resolve(3);
 }
-function fnRej() {
+async function fnRej() {
+	await wait(1000);
 	return Promise.reject(new Error("Was rejected"));
 }
 
 async function main() {
-	console.log(await promisAll[(fn1, fn2, fn3)]); // [1, 2, 3]
-	console.log(await promisAll[(fn1, fn3)]); // [1, 3]
-	console.log(await promisAll[(fn1, fnRej)]); // "Was rejected"
+	console.log(await promisAll([fn1, fn2, fn3])); // [1, 2, 3]
+	console.log(await promisAll([fn1, fn3])); // [1, 3]
+	console.log(await promisAll([fn1, fnRej])); // "Was rejected"
 }
 
 main();
