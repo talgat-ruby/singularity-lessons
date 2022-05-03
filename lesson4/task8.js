5d7a1154a3248101cb53407437cd38932aae70e9
Talgat S <talgat.s@protonmail.com>
2022-05-03 11:29:32 +0600

    add solutions to tasks from lesson4

--- lesson4/task8.js
+++ lesson4/task8.js
@@ -1,8 +1,20 @@
-class Polling {
-	constructor(fn, delay, atempts) {}
-}
-
 const wait = (delay) => new Promise((res) => setTimeout(res, delay));
+
+class Polling {
+	constructor(fn, delay, attempts) {
+		this.fn = fn;
+		this.delay = delay;
+		this.attempts = attempts;
+	}
+
+	async start() {
+		for (let i = 0; i < this.attempts; i++) {
+			console.log("Call func");
+			await Promise.all([this.fn(), wait(this.delay)]);
+			console.log("Return func");
+		}
+	}
+}
 
 async function fn1() {
 	await wait(1000);
@@ -14,8 +26,8 @@ async function fn2() {
 	return Promise.resolve(1);
 }
 
-const poll = new Polling(fn1, 2000, 4);
-poll.start(); // 4 times each 2s delay
+// const poll = new Polling(fn1, 2000, 4);
+// poll.start(); // 4 times each 2s delay
 
 const poll2 = new Polling(fn2, 2000, 4);
 poll2.start(); // 4 times each 4s delay
